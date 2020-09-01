38e81e9a52cf8f456a59a55197cf6e4ba0c4f544
Jesús Wisord <wisord.autumn@gmail.com>
2020-09-01 15:53:27 -0500

    Deleting unnecesary files

--- src/components/HelloWorld.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import style from  './styles/HelloWorld.module.scss'
-const HelloWorld = () => {
-  return (
-    <h1 className={style.title}>Hola mundo!</h1>
-  )
-}
-
-export default HelloWorld
